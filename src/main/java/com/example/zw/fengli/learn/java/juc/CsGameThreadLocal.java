397f51d2eb85e3f1d2e53219308e6ac65f69c042
wudongfeng <wdfbit@163.com>
2020-09-05 14:48:44 +0800

    提交threadLocal

---
+++ src/main/java/com/example/zw/fengli/learn/java/juc/CsGameThreadLocal.java
@@ -0,0 +1,13 @@
+package com.example.zw.fengli.learn.java.juc;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class CsGameThreadLocal {
+    private static final Integer BULLET_NUMBER = 1500;
+    private static final Integer KILLED_ENEMIES = 0;
+    private static final Integer LIFE_VALUE = 0;
+    private static final Integer TOTAL_PLAYERS = 10;
+    private static final ThreadLocalRandom RANDOM = ThreadLocalRandom.current();
+
+
+}

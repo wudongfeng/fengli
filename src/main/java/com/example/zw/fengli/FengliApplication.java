d7bd5e6e85defa90003edbb362feadf000722080
wudongfeng <wudongfeng@wudongfeng-Mac.local>
2020-09-05 14:24:02 +0800

    初始化项目

---
+++ src/main/java/com/example/zw/fengli/FengliApplication.java
@@ -0,0 +1,13 @@
+package com.example.zw.fengli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FengliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FengliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zw/fengli/FengliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zw.fengli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FengliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
